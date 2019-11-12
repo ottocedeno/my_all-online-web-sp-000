73e0eddeb7df9f34ddddf78810e571e1b16ba794
Otto Cedeno <otto@ottostacos.com>
2019-11-12 19:56:54 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,12 +2,8 @@ require 'pry'
 
 def my_all?(collection)
   i = 0
-  all_true = true
-  while i < collection.length
-    if !yield(collection[i])
-      all_true = false
-    end
-    i += 1
+  block_return_values
+  while condition
+
   end
-  return all_true
 end
