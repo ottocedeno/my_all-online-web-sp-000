401c5e7c09a4aff86f8d71c47410d390996bdb97
Otto Cedeno <otto@ottostacos.com>
2019-11-12 19:57:45 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -3,7 +3,8 @@ require 'pry'
 def my_all?(collection)
   i = 0
   block_return_values = Array.new
-  while condition
-
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    i += 1
   end
 end
